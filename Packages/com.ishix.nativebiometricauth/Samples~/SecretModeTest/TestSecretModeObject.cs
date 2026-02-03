d819a874717b0a0d4ce7b208bd3767e558fecb2f
IShix <49128115+IShix-g@users.noreply.github.com>
2026-02-03 23:50:58 +0900

    feat: update TestSecretModeObject to use localized error messages and adjust prefab UI settings

--- Packages/com.ishix.nativebiometricauth/Samples~/SecretModeTest/TestSecretModeObject.cs
+++ Packages/com.ishix.nativebiometricauth/Samples~/SecretModeTest/TestSecretModeObject.cs
@@ -1,5 +1,4 @@
 
-using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,12 +6,10 @@ namespace NativeBiometricAuth.Samples
 {
     internal sealed class TestSecretModeObject : MonoBehaviour, ISecretModeObject
     {
-        static readonly Regex s_pascalCaseRegex = new ("([a-z])([A-Z])", RegexOptions.Singleline);
-        
         [SerializeField] RectTransform _lockOverlay;
         [SerializeField] Text _errorText;
         [SerializeField] SecretModeFailureAlertLegacy _failureAlert;
-
+        
         void Awake()
         {
             _lockOverlay.gameObject.SetActive(false);
@@ -36,14 +33,12 @@ namespace NativeBiometricAuth.Samples
         public void OnFailure(BiometricFailureReason reason)
         {
             _errorText.gameObject.SetActive(true);
-            var reasonString = SplitPascalCase(reason.ToString());
+            var reasonString = BiometricSettings.Instance.GetMessage(reason, Application.systemLanguage);
             _errorText.text = reasonString;
             if (reason != BiometricFailureReason.Canceled)
             {
                 _failureAlert.Show(reason, reasonString);
             }
         }
-        
-        static string SplitPascalCase(string input) => s_pascalCaseRegex.Replace(input, "$1 $2");
     }
 }
